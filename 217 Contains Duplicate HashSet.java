3034fa2d933764fa0ce42bc2a057ec70a52a08a4
WeiquanMai <weiquanmai@gmail.com>
2025-09-29 11:20:17 -0400

    Added header comments to files.

--- 217 Contains Duplicate HashSet.java
+++ 217 Contains Duplicate HashSet.java
@@ -1,3 +1,16 @@
+/*
+    Problem 217: Contains Duplicate
+    URL: https://leetcode.com/problems/contains-duplicate/description/
+
+    Approach:
+    Create a HashSet.
+    Iterate through the array. If the set contains the element in the array, return true.
+    Otherwise, add the element to the set.
+    If we iterate through the array, and there are no matching sets, return false.
+
+    Time Complexity: O(n)- We iterate through the array once
+    Space Complexity: O(n)- In worst case, we store all the elements of the array into HashSet.
+*/
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         // Create a HashSet
--- 217 Contains Duplicate Sorted Array.java
+++ 217 Contains Duplicate Sorted Array.java
@@ -1,3 +1,16 @@
+/*
+    Problem 217: Contains Duplicate
+    URL: https://leetcode.com/problems/contains-duplicate/description/
+
+    Approach:
+    Sort the array, and then iterate through the array using a for loop.
+    Since the array is sorted, we can compare nums[i] to nums[i + 1] to see if there is a matching pair.
+    If matching pair exists, return true.
+    If we iterate through the entire array, and no matching pairs exist, return false.
+
+    Time Complexity: O(nlogn) - We sort through the list, and iterate through list once.
+    Space Complexity: O(1) - We utilize space already dedicated to the list.
+*/
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         // Sort the array
